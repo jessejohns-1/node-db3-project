85af8709b0c6824893c988cc74653ef15ad636da
Jesse <eoshout@Gmail.com>
2021-09-08 16:36:00 -0400

    findsteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -127,7 +127,7 @@ async function findById(scheme_id) { // EXERCISE B
     return result
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -148,6 +148,16 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+
+    const results = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('st.step_number')
+      if(!results[0].step_id) return []
+      else{
+        return results
+      }
 }
 
 function add(scheme) { // EXERCISE D
